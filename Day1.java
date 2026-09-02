a4b86e3f5ff27a2a8fad152a860dfcba204bcb33
MUH-ARYA-MARUF <aryamaruf812@gmail.com>
2026-09-02 21:33:39 +0800

    Create Day1.java

---
+++ Day1.java
@@ -0,0 +1,13 @@
+/** 
+ * Komentar ini digunakan untuk memberikan 
+ * keterangan atau penjelasan tentang program.
+ */
+public class arya {
+     // Komentar ini digunakan untuk memberikan catatan atau penjelasan tentang kode yang ada setelahnya.
+     public static void main(String[] args) {
+        /* 
+        * Komentar ini hanya digunakan untuk memberikan catatan atau penjelasan pada kode.
+        */
+         System.out.println("Saya sedang ngoding day1"); 
+        } 
+    }
