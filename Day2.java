9cb8d103f1539c7deb538ca501c331c566ed75d3
MUH-ARYA-MARUF <aryamaruf812@gmail.com>
2026-09-03 20:46:57 +0800

    Create Day2.java

---
+++ Day2.java
@@ -0,0 +1,19 @@
+public class day2 {
+    public static void main(String[] args) {
+        String nama = "MUH ARYA MA'RUF";
+        String nim = "D0225001";
+        int umur = 20 ;
+        String jurusan = "Informatika";
+
+        System.out.println("===================");
+        System.out.println("    BIODATA DIRI");
+        System.out.println("===================");
+        System.out.printf("Nama    : %s%nNIM     : %s%nUmur    : %d tahun%n", nama , nim, umur);
+        System.out.print("Jurusan : ");
+        System.out.println(jurusan);
+        
+        
+
+    }
+    
+}
