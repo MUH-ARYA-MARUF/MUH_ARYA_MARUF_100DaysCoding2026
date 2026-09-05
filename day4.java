8e7992f0980c6e3a0e311a0e27d13ffd5b0cce7c
MUH-ARYA-MARUF <aryamaruf812@gmail.com>
2026-09-05 23:34:57 +0800

    Create day4.java

---
+++ day4.java
@@ -0,0 +1,18 @@
+public class day4 {
+    public static void main(String[] args) {
+        int a = 100;
+        short b = 10;
+        byte c = 20;
+        long d = 10000;
+
+        System.out.println("nilai dari int : "+ a);
+        System.out.println("nilai dari short : "+ b);
+        System.out.println("nilai dari byte : "+ c);
+        System.out.println("nilai dari long : "+ d);
+
+        //int paling sering dipakai di Java
+        //short dan byte cocok untuk menghemat memori
+        //long digunakan apabila nilai di int sudah memenuhi kapasitas
+    }
+    
+}
