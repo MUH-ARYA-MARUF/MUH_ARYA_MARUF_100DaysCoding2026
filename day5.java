97084e408c26e2ad53418d75e8515f89d46fd1c9
MUH-ARYA-MARUF <aryamaruf812@gmail.com>
2026-09-06 20:29:57 +0800

    Create day5.java

---
+++ day5.java
@@ -0,0 +1,12 @@
+public class day5 {
+      public static void main(String[] args) {
+        float a = 50.5f;
+        double b = 150.1;
+
+        System.out.println("Berat badan  : " + a);
+        System.out.println("Tinggi badan : " + b);
+
+        //float untuk menyimpan angka desimal, tetapi kapasitas dan ketelitiannya lebih terbatas dan harus diberi huruf f dibelakangnya.
+        //double digunakan unntuk menyimpan angka desimal, tetapi memiliki ketelitian dan kapasitas lebih besar daripada float
+    }
+}
