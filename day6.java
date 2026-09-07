a6beddaea49e3787c9410baf2f3ff3dad662f5dd
MUH-ARYA-MARUF <aryamaruf812@gmail.com>
2026-09-07 20:38:50 +0800

    Create day6.java

---
+++ day6.java
@@ -0,0 +1,12 @@
+public class day6 {
+    public static void main(String[] args) {
+        char huruf = 'A';
+        //untuk menyimpan satu karakter ditulis menggunakan satu tanda petik ('').
+        boolean HidupTidak = true;
+        //boolean hanya mengenal true and false dan digunakan untuk mengambil keputusan.
+
+        System.out.println("Inisial saya = "+huruf);
+        System.out.println("Saya hidup = "+HidupTidak);
+    }
+    
+}
