e3d1837a298df1d624ef0341f6cf3d631c24f789
MUH-ARYA-MARUF <aryamaruf812@gmail.com>
2026-09-08 20:16:31 +0800

    Create day7.java

---
+++ day7.java
@@ -0,0 +1,10 @@
+public class day7 {
+    public static void main(String[] args) {
+        String nama = "MUH ARYA MA'RUF";
+
+        System.out.println("Nama saya "+nama);
+
+    //string digunakan untuk memasukkan teks dalam subuah java
+    }
+    
+}
