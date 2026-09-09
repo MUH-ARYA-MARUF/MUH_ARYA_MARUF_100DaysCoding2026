de96b3c56f258feaf76e1ff17fd671d0c7967367
MUH-ARYA-MARUF <aryamaruf812@gmail.com>
2026-09-09 23:14:09 +0800

    Create day8.java

---
+++ day8.java
@@ -0,0 +1,13 @@
+public class day08 {
+
+    public static void main(String[] args) {
+        //deklarasi 
+        String nama;
+        //inisialisasi
+        nama = "Aco";
+             System.out.println("Nama sebelum diganti " + nama);
+        //update
+        nama = "Acong";
+             System.out.println("Nama setelah diganti " + nama);
+    }
+}
