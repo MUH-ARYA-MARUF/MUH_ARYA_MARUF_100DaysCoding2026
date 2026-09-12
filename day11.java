e603f8c7de01951ffa1e2434bb40ef77315973de
MUH-ARYA-MARUF <aryamaruf812@gmail.com>
2026-09-12 20:35:36 +0800

    Create day11.java

---
+++ day11.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+
+public class day11{
+
+            public static void main(String[] args){
+                Scanner am = new Scanner(System.in);
+                System.out.print("Siapa nama anda : ");
+                String nama = am.nextLine();
+                System.out.println("-----------------------");
+                System.out.println("Nama saya : " +nama);
+
+
+                }
+            }
