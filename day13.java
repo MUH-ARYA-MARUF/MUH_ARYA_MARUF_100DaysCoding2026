b503f4166d97d357513a709b210ed3d52a997b50
MUH-ARYA-MARUF <aryamaruf812@gmail.com>
2026-09-14 22:31:53 +0800

    Create day13.java

---
+++ day13.java
@@ -0,0 +1,11 @@
+public class arya1 {
+    public static void main(String[] args) {
+        char hadir = 'H';
+        boolean statusHadir = (hadir == 'X');
+
+        System.out.println("Status hadir : "+ statusHadir);
+
+
+    }
+
+}
