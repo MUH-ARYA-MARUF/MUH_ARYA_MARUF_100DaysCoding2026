c73554aa380ad8910969c4d83295b85c8a2cb639
MUH-ARYA-MARUF <aryamaruf812@gmail.com>
2026-09-16 23:02:45 +0800

    Create day15.java

---
+++ day15.java
@@ -0,0 +1,16 @@
+public class day15 {
+    public static void main(String[] args) {
+        //soal 3
+        double harga = 15000;
+        double jumlahBarang = 4;
+        double totalHarga = harga * jumlahBarang;
+            System.out.println("Total harga : "+ totalHarga);
+
+        //soal 4
+        double a = 10;
+        double b = 0;
+            System.out.println("Hasil pembagian : "+ (a / b));
+        
+    }
+    
+}
