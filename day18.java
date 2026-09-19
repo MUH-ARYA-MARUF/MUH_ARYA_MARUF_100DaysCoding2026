14fee7c646433e916e276ed295235e46e0b16c6d
MUH-ARYA-MARUF <aryamaruf812@gmail.com>
2026-09-19 22:50:45 +0800

    Create day18.java

---
+++ day18.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int angka = 100;
+
+        // Konversi otomatis dari int ke long
+        long angkaLong = angka;
+
+        // Konversi otomatis dari long ke float
+        float angkaFloat = angkaLong;
+
+        // Konversi otomatis dari float ke double
+        double angkaDouble = angkaFloat;
+
+        System.out.println("Nilai int    : " + angka);
+        System.out.println("Nilai long   : " + angkaLong);
+        System.out.println("Nilai float  : " + angkaFloat);
+        System.out.println("Nilai double : " + angkaDouble);
+    }
+}
