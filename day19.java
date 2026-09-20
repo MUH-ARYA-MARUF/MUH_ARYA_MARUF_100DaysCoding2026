f106ffc72279945d82fb1b3ac246ebcbe2d1a1b1
MUH-ARYA-MARUF <aryamaruf812@gmail.com>
2026-09-20 22:21:35 +0800

    Create day19.java

---
+++ day19.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        double nilaiBesar = 85.75;
+
+        // Konversi manual dari double ke int
+        int nilaiKecil = (int) nilaiBesar;
+        System.out.println("Nilai double : " + nilaiBesar);
+        System.out.println("Nilai int    : " + nilaiKecil);
+    }
+}
