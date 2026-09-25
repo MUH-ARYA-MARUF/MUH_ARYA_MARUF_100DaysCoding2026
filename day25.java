16e2e53dc30e49b654d405dba90d8b6aa18d5819
MUH-ARYA-MARUF <aryamaruf812@gmail.com>
2026-09-25 19:14:41 +0800

    Create day25.java

---
+++ day25.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Day25 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        double r;
+        double luas;
+
+        System.out.print("Masukkan jari-jari : ");
+        r = input.nextDouble();
+
+        luas = 3.14 * r * r;
+
+        System.out.println("Jadi, luas lingkaran adalah : " + luas);
+    }
+}
